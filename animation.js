3545fbbb29a72cca9caf38ac1a4045a35c4a739b
João Vítor <jvpeniza@gmail.com>
2023-10-06 09:44:52 -0300

    Criando função Animate()

--- animation.js
+++ animation.js
@@ -1,42 +1,24 @@
-const optionsText1 = { 
-    delay: 100, 
-    duration: 2000,
-    reset: false,
-    origin: 'left',
-    distance: '5rem'
+function Animate(dealy, duration, reset, origin, distance){
+    const objAnimate = {
+        delay: dealy, 
+        duration: duration,
+        reset: reset,
+        origin: origin,
+        distance: distance
+    };
+
+    return objAnimate;
 }
 
-const optionsText2 = { 
-    delay: 100, 
-    duration: 2000,
-    reset: false,
-    origin: 'right',
-    distance: '5rem'
-}
+const optionsText1 = Animate(100, 2000, false, 'left', '5rem');
 
-const optionsCircle1 = {
-    delay: 900, 
-    duration: 3000,
-    reset: false,
-    origin: 'top',
-    distance: '5rem'
-}
+const optionsText2 = Animate(100, 2000, false, 'right', '5rem');
 
-const optionsCircle2 = {
-    delay: 900, 
-    duration: 2000,
-    reset: false,
-    origin: 'top',
-    distance: '5rem'
-}
+const optionsCircle1 = Animate(900, 3000, false, 'top', '5rem');
 
-const optionsImgDog = {
-    delay: 100, 
-    duration: 1000,
-    reset: false,
-    origin: 'bottom',
-    distance: '5rem'
-}
+const optionsCircle2 = Animate(900, 2000, false, 'top', '5rem');
+
+const optionsImgDog = Animate(100, 1000, false, 'bottom', '5rem');
 
 ScrollReveal(optionsText1).reveal('.reveal-text1');
 ScrollReveal(optionsText2).reveal('.reveal-text2');
@@ -45,3 +27,4 @@ ScrollReveal(optionsCircle1).reveal('.reveal-circle1');
 ScrollReveal(optionsCircle2).reveal('.reveal-circle2');
 
 ScrollReveal(optionsImgDog).reveal('.dog');
+
